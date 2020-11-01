5bf8094dd079427b70d29be9e04bf0ab6215e4b2
ZhangZhiHao233 <791745123@qq.com>
2020-11-01 16:56:06 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -29,12 +29,12 @@ def set_projectpath(current_path):
 
 
 def concatImage(images,mode="L"):
-	if not isinstance(images, list):
-		raise Exception('images must be a  list  ')
-	count=len(images)
-	size= Image.fromarray(images[0]).size
-	target = Image.new(mode, (size[0] * count, size[1] * 1))
-	for i  in  range(count):
-		image = Image.fromarray(images[i]).resize(size, Image.BILINEAR)
-		target.paste(image, (i*size[0], 0, (i+1)*size[0], size[1]))
-	return target
+    if not isinstance(images, list):
+        raise Exception('images must be a  list  ')
+    count=len(images)
+    size= Image.fromarray(images[0]).size
+    target = Image.new(mode, (size[0] * count, size[1] * 1))
+    for i  in  range(count):
+        image = Image.fromarray(images[i]).resize(size, Image.BILINEAR)
+        target.paste(image, (i*size[0], 0, (i+1)*size[0], size[1]))
+    return target
