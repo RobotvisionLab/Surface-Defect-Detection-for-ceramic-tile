1c3b73be2a53c28fa2b7293d611a6dcb060d7341
ZhangZhiHao233 <791745123@qq.com>
2020-11-19 15:49:48 +0800

    Update postprocess.py
    增加postprocess模块：
    1 分割为4*22小区域
    2 小区域内按阈值分为全白、全黑
    3 每一列按小块数量的阈值设为全白、全黑

--- postprocess.py
+++ postprocess.py
@@ -12,9 +12,17 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 SPLIT_WIDTH = 512
 SPLIT_HEIGHT = 1408
-
 SPLIT_ROWS = 10
 SPLIT_COLS = 28
+
+THRESHOLD_BINARYZATION = 0.1
+THRESHOLD_POSTPROCESS = 0.4
+THRESHOLD_COLSPIX = 0.25
+
+SPLIT_WIDTH_POSTPROCESS = 4
+SPLIT_HEIGHT_POSTPROCESS = 22
+SPLIT_ROWS_POST_PROCESS = 8
+SPLIT_COLS_POST_PROCESS = 16
 
 #function: 读取大图，先转为灰度并保存，然后分割成小图
 #          分割的行数、列数向下取整，会丢失一部分边界像素
@@ -100,7 +108,8 @@ def detectbmp(pbPath, bmpPath):
 #bmpjointPath: 识别后拼接保存的图片目录
 #rows:  大图中每列几个小图
 #cols:  大图中每行几个小图
-def detectbmps(pbPath, bmpsPath, bmpDetectedPath, bmpjointPath, cols, rows):
+def detectbmps(pbPath, bmpsPath, bmpDetectedPath, bmpDetectedPath_nopost, bmpjointPath,
+               bmpjointPath_nppost, cols, rows):
     start = time.clock()
     with tf.Graph().as_default():
         output_graph_def = tf.GraphDef()
@@ -115,6 +124,7 @@ def detectbmps(pbPath, bmpsPath, bmpDetectedPath, bmpjointPath, cols, rows):
             img_list = [Image.open(bmpsPath + fn) for fn in listdir(bmpsPath)]
             width, height = img_list[0].size
             result = Image.new(img_list[0].mode, (width * cols, height * rows))
+            result_nopost = Image.new(img_list[0].mode, (width * cols, height * rows))
 
             pic_index = 0
             for i in range(cols):
@@ -127,31 +137,117 @@ def detectbmps(pbPath, bmpsPath, bmpDetectedPath, bmpjointPath, cols, rows):
                     print("detect time: ", interval_2 - interval_1)
                     print("detect-->", pic_index)
                     mask = np.squeeze(out)
+                    mask_resize_nopost = cv2.resize(mask, (width, height))
+
+                    mask = postprocess(mask, THRESHOLD_BINARYZATION, THRESHOLD_POSTPROCESS)
                     mask_resize = cv2.resize(mask, (width, height))
+
                     save_path = bmpDetectedPath + str(pic_index).zfill(4) + ".bmp"
-                    cv2.imwrite(save_path, mask_resize*255)
+                    save_path_nopost = bmpDetectedPath_nopost + str(pic_index).zfill(4) + ".bmp"
+
+                    cv2.imwrite(save_path, mask_resize)
+                    cv2.imwrite(save_path_nopost, mask_resize_nopost*255)
+
                     print("save-->", pic_index)
-                    result.paste(Image.fromarray(mask_resize*255), (i * width, j * height))
+                    result.paste(Image.fromarray(np.uint8(mask_resize)), (i * width, j * height))
+                    result_nopost.paste(Image.fromarray(np.uint8(mask_resize_nopost*255)), (i * width, j * height))
                     print("paste-->", pic_index)
                     pic_index = pic_index + 1
 
             end = time.clock()
             print("total time: ", end-start)
             result.save(bmpjointPath)
+            result_nopost.save(bmpjointPath_nppost)
             print("joint done.")
 
 
+#function: 后处理, 对图片分小块,对小块的里的像素先进行二值化(0-1),若1的个数>阈值,则该区域都为1,否则为0
+#img: 待处理图片数据 np.arrary
+#thread_binary： 二值化阈值
+#thread_postprocess： 后处理阈值
+def postprocess(img, thread_binary=0.1, thread_postprocess=0.5):
+    image = ImageBinarization(img, thread_binary)
+    image = Image.fromarray(np.uint8(image), mode='L')
+    #4*16 22*8
+    result = Image.new('L', (SPLIT_WIDTH_POSTPROCESS * SPLIT_COLS_POST_PROCESS,
+                             SPLIT_HEIGHT_POSTPROCESS * SPLIT_ROWS_POST_PROCESS))
+
+    crops_imgs = []
+    index = 0
+    for x in range(SPLIT_COLS_POST_PROCESS):
+        for y in range(SPLIT_ROWS_POST_PROCESS):
+            rect = (x*SPLIT_WIDTH_POSTPROCESS, y*SPLIT_HEIGHT_POSTPROCESS,
+                    (x+1)*SPLIT_WIDTH_POSTPROCESS,(y+1)*SPLIT_HEIGHT_POSTPROCESS)
+            region = image.crop(rect)
+            index = index + 1
+            crops_imgs.append(region)
+            #print("index: ", index)
+
+    print(len(crops_imgs))
+    thr = SPLIT_WIDTH_POSTPROCESS * SPLIT_HEIGHT_POSTPROCESS * thread_postprocess
+    print(thr)
+    cols_count = 0
+    cols_count_pix = 0
+    for i in range(len(crops_imgs)):
+        cols_count = cols_count + 1
+        im = np.array(crops_imgs[i])
+        #print(im)
+        count_pix1 = sum(sum(im == 255))
+
+        if count_pix1 >= thr:
+            #im = np.where(im == 0, 255, 255)
+            cols_count_pix = cols_count_pix + 1
+            print("> thr: ", count_pix1)
+        #else:
+        #    #im = np.where(im == 255, 0, 0)
+
+        if cols_count % SPLIT_ROWS_POST_PROCESS == 0:
+            print("cols_count: ", cols_count, "thr: ", SPLIT_ROWS_POST_PROCESS*THRESHOLD_COLSPIX)
+            print("cols_count_pixs: ", cols_count_pix)
+            if cols_count_pix > SPLIT_ROWS_POST_PROCESS*THRESHOLD_COLSPIX:
+                for i in range(cols_count - SPLIT_ROWS_POST_PROCESS, cols_count):
+                    img = np.array(crops_imgs[i])
+                    im = np.where(img == 0, 255, 255)
+                    crops_imgs[i] = Image.fromarray(np.uint8(im), mode='L')
+                    print("set 1")
+            else:
+                for i in range(cols_count - SPLIT_ROWS_POST_PROCESS, cols_count):
+                    img = np.array(crops_imgs[i])
+                    im = np.where(img == 255, 0, 0)
+                    crops_imgs[i] = Image.fromarray(np.uint8(im), mode='L')
+                    print("set 0")
+
+            cols_count_pix = 0
 
 
+    count = 0
+    for i in range(SPLIT_COLS_POST_PROCESS):
+        for j in range(SPLIT_ROWS_POST_PROCESS):
+            result.paste(crops_imgs[count], (i * SPLIT_WIDTH_POSTPROCESS, j * SPLIT_HEIGHT_POSTPROCESS))
+            #print("joint-->", count)
+            count = count + 1
+
+    return np.array(result)
+
+
+
+
+
+def ImageBinarization(img, threshold=0.1):
+    image = np.where(img > threshold, 255, 0)
+    return image
 
 if __name__ == '__main__':
-    #splitBMP("222.bmp", "222_gray.bmp", "./222/")
-    #jointBMP("./222/", "222_gray_joint.bmp", SPLIT_ROWS, SPLIT_COLS)
+    splitBMP("555.bmp", "./test/555/555_gray.bmp", "./test/555/")
+    jointBMP("./test/555/", "./test/555_gray_joint.bmp", SPLIT_ROWS, SPLIT_COLS)
+    detectbmps("ckp-174.pb", "./test/555", "./test/555_detected/", "./test/555_detected_nopost/",
+               "./test/555_detected_jointed.bmp", "./test/555_detected_jointed_nopost.bmp", 28, 10)
     #img = detectbmp("ckp-174.pb","./222/0013.bmp")
     #cv2.imwrite("outbmp.bmp", img * 255)
-    detectbmps("ckp-174.pb", "./222/", "./222_detected/", "222_detected_jointed.bmp", 28, 10)
-
-
+    #detectbmps("ckp-174.pb", "./222/", "./222_detected/", "222_detected_jointed.bmp", 28, 10)
+    #img = detectbmp("ckp-174.pb", "./222/0017.bmp")
+    #cv2.imwrite("asd.bmp", img*255)
+    #postprocess(img)
 
 
 
